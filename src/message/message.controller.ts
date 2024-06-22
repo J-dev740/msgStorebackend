1fb8bb2e284024d7ebaa41a5292c7df2a81f2304
J-dev740 <tcr19ec029@gectcr.ac.in>
2024-06-23 01:40:06 +0530

    update: added comments to the message controller

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -3,16 +3,20 @@ import { MessageService } from './message.service';
 
 @Controller('api/message')
 export class MessageController {
+    // instatiation of message service to use it's methods inside the controller endpoints
     constructor(private readonly messageService:MessageService){}
 
 
      @Get()
+    //  get method to get message from the contract 
      async getMessage(){
         const message= await this.messageService.getMessage();
         return {message};
      }
 
      @Post()
+    //  set method 
+    // only admin modifier specified in the smart contract 
      async setMessage(@Body('message') message:string){
         await this.messageService.setMessage(message);
         return {status:'success'};
