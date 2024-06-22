b459ae8f3ef896a0411b85aee0de386cc3f50d55
J-dev740 <tcr19ec029@gectcr.ac.in>
2024-06-23 00:50:11 +0530

    refact: refactored messageService with dotenv config

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import {ethers} from  'ethers';
+import * as dotenv from 'dotenv';
 
-const contractAddress='0x4b6A4A8c45626c1a902127Bb8574cd317FdFCda4';
+dotenv.config()
+
+// const contractAddress='0x4b6A4A8c45626c1a902127Bb8574cd317FdFCda4';
+const contractAddress=process.env.CONTRACT_ADDRESS;
+
 const contractABI=[
     {
       "inputs": [
@@ -48,8 +53,8 @@ export class MessageService {
     private contract:ethers.Contract;
 
     constructor(){
-        this.provider= new ethers.InfuraProvider('sepolia','00173c97e60d44ba80bdb4291ce2bf45')
-        this.signer=  new ethers.Wallet('adc627587063d15a2ea7e979d3b85c7fea2a0b08c3c6737302c4d28fa8b30a80',this.provider)
+        this.provider= new ethers.InfuraProvider('sepolia',process.env.INFURA_PROJECT_ID)
+        this.signer=  new ethers.Wallet(process.env.PRIVATE_KEY,this.provider)
         this.contract= new ethers.Contract(contractAddress,contractABI,this.signer);
 
     }
