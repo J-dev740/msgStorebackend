0f8b172ca00316e3c0162a46c397635832b1b7f5
J-dev740 <tcr19ec029@gectcr.ac.in>
2024-06-23 01:38:25 +0530

    added comments to message service methods

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -6,7 +6,7 @@ dotenv.config()
 
 // const contractAddress='0x4b6A4A8c45626c1a902127Bb8574cd317FdFCda4';
 const contractAddress=process.env.CONTRACT_ADDRESS;
-
+// contracdt abi
 const contractABI= [
     {
       "inputs": [
@@ -135,22 +135,29 @@ const contractABI= [
   ];
 @Injectable()
 export class MessageService {
+    // declaring provider,signer and contract with their types 
     private provider: ethers.JsonRpcProvider;
     private signer: ethers.Signer;
     private contract:ethers.Contract;
 
+    // initializing the contract signer and provider 
     constructor(){
+
         this.provider= new ethers.InfuraProvider('sepolia',process.env.INFURA_PROJECT_ID)
         this.signer=  new ethers.Wallet(process.env.PRIVATE_KEY,this.provider)
         this.contract= new ethers.Contract(contractAddress,contractABI,this.signer);
 
     }
+    // method provided by the messeage service to retrive the messgae from smart contract
     async getMessage():Promise<string>{
         const result= await this.contract.getMessage();
         return result;
     }
+    // method provided by the message service to update the message 
+    // only admin can executre this endpoint 
     async setMessage(newMessage:string):Promise<void>{
         const tx= await this.contract.setMessage(newMessage);
+        // waiting for the transaction to go through 
         await tx.wait();
     }
 }
