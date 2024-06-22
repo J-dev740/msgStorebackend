50be4eb2573315cf10b0594e5de9122e21d0018e
J-dev740 <tcr19ec029@gectcr.ac.in>
2024-06-23 00:25:35 +0530

    complete getMessage() and setMessage() methods in the msg service provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessageService } from './message/message.service';
+import { MessageController } from './message/message.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, MessageController],
   providers: [AppService, MessageService],
 })
 export class AppModule {}
--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -40,6 +40,25 @@ const contractABI=[
       "stateMutability": "view",
       "type": "function"
     }
-  ]
+  ];
 @Injectable()
-export class MessageService {}
+export class MessageService {
+    private provider: ethers.JsonRpcProvider;
+    private signer: ethers.Signer;
+    private contract:ethers.Contract;
+
+    constructor(){
+        this.provider= new ethers.InfuraProvider('sepolia','00173c97e60d44ba80bdb4291ce2bf45')
+        this.signer=  new ethers.Wallet('adc627587063d15a2ea7e979d3b85c7fea2a0b08c3c6737302c4d28fa8b30a80',this.provider)
+        this.contract= new ethers.Contract(contractAddress,contractABI,this.signer);
+
+    }
+    async getMessage():Promise<string>{
+        const result= await this.contract.getMessage();
+        return result;
+    }
+    async setMessage(newMessage:string):Promise<void>{
+        const tx= await this.contract.setMessage(newMessage);
+        await tx.wait();
+    }
+}
