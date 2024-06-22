f3ad4475753c9b84cec2218feb49853ce0cd6d8a
J-dev740 <tcr19ec029@gectcr.ac.in>
2024-06-23 00:00:40 +0530

    deploy contract on sepolia testnet and record abi

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -1,6 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import {ethers} from  'ethers';
 
-
+const contractAddress='0x4b6A4A8c45626c1a902127Bb8574cd317FdFCda4';
+const contractABI=[
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "newMsg",
+          "type": "string"
+        }
+      ],
+      "name": "setMessage",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "initialMsg",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+      "name": "getMessage",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ]
 @Injectable()
 export class MessageService {}
