b70d726320c9652c634495b6e8d1f8183ef74f67
J-dev740 <tcr19ec029@gectcr.ac.in>
2024-06-23 01:11:03 +0530

    added updated contract MessageStorage with openzepplin integration

--- contract/MessageStorage.sol
+++ contract/MessageStorage.sol
@@ -1,23 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
-contract MessageStorage {
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract MessageStorage is Ownable {
     string private message;
-    address  private admin;
-
-    constructor (string memory initialMsg){
+    // address  private admin;
+    event MessageUpdated(string newMessage);
+    constructor (string memory initialMsg) Ownable(msg.sender){
         message=initialMsg;
-        admin =msg.sender;
+        // admin =msg.sender;
+        emit MessageUpdated(initialMsg);
     }
 
     function getMessage() public view returns (string memory){
         return message;
     }
-    function setMessage(string memory newMsg) public{
+    function setMessage(string memory newMsg) external onlyOwner {
 
-        require(msg.sender==admin,"Only admin can  set the message");
+        // require(msg.sender==admin,"Only admin can  set the message");
         message =newMsg;
-        
+        emit MessageUpdated(newMsg);
+
 
     }
 }
